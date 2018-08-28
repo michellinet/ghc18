2cdb9ab66e51324739fec742bae0a915731a7c14
michellinet <michellinet@procore.com>
2018-08-27 21:11:10 -0700

    Set up Xcode project

---
+++ GHC18/GHC18/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GHC18
+//
+//  Created by Michelline Tran on 8/27/18.
+//  Copyright © 2018 Michelline Tran. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
